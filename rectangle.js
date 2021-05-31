b24ded644867ebd63ce935ab1b203583a5453052
peterhu <chu3@mail.sfsu.edu>
2021-05-30 21:28:18 -0700

    callback implementation

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,22 @@
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => x*y;
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(_ =>
+            callback(new Error("Rectangle dimensions should be greater than zero:  x = "
+                + x + ",  and y = " + y), null),
+            2000);
+    }
+    else {
+        setTimeout(_ =>
+            callback(null,
+                {
+                    perimeter: _ => (2 * (x + y)),
+                    area: _ => x * y
+                }),
+            2000);
+    }
+
+}
+
+
+
+
