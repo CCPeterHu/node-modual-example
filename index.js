d802025c80d3402c646c3909c9260059dfa75cfe
CCCPeterHu <chu3@mail.sfsu.edu>
2021-06-03 10:57:38 -0700

    node MongoDB add methods for database operations

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
+
 
 const url = 'mongodb://localhost:27017'
 const dbname = 'conFusion';
@@ -11,28 +13,51 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to the server');
 
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
 
-    collection.insertOne({"name": "Peter Hu", "description": "test description"}, 
-    (err, result)=> {
-        assert.equal(err, null);
+    dboper.insertDocument(db, {name: "Peter", description: "haha description"}, 'dishes', result => {
+        
+        console.log('Insert Document:\n', result.ops);
+        
+        dboper.findDocuments(db, 'dishes', docs => {
+            console.log('Found Documents:\n', docs);
 
-        console.log('After Inserting: \n');
-        console.log(result.ops);
+            dboper.updatetDocument(db, {name: "Peter"}, {description: "Updated Test"}, 'dishes', result => {
+                console.log('Updated Document:\n', result.result);
 
-        collection.find({}).toArray((err, docs)=> {
-            assert.equal(err, null);
+                dboper.findDocuments(db, 'dishes', docs => {
+                    console.log('Found Documents:\n', docs);
 
-            console.log('Found: \n');
-            console.log(docs);
+                    db.dropCollection('dishes', result =>{
+                        console.log('Dropped Collection: ', result);
 
-
-            db.dropCollection('dishes', (err, result)=>{ // remove from db
-                assert.equal(err, null);
-
-                client.close();
+                        client.close();
+                    });
+                });
             });
         });
     });
+    // const collection = db.collection('dishes');
+
+    // collection.insertOne({"name": "Peter Hu", "description": "test description"}, 
+    // (err, result)=> {
+    //     assert.equal(err, null);
+
+    //     console.log('After Inserting: \n');
+    //     console.log(result.ops);
+
+    //     collection.find({}).toArray((err, docs)=> {
+    //         assert.equal(err, null);
+
+    //         console.log('Found: \n');
+    //         console.log(docs);
+
+
+    //         db.dropCollection('dishes', (err, result)=>{ // remove from db
+    //             assert.equal(err, null);
+
+    //             client.close();
+    //         });
+    //     });
+    // });
 
 });
