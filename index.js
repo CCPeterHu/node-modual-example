fb40628a947f312d3a89e5b9c22a5c0095c13ef5
chen <chu3@mail.sfsu.edu>
2021-06-03 14:36:30 -0700

    Node Callback Hell and Promises better format

--- index.js
+++ index.js
@@ -6,35 +6,38 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017'
 const dbname = 'conFusion';
 
-MongoClient.connect(url).then((client) => {
+MongoClient.connect(url).then(client => {
 
     console.log('Connected correctly to server');
     const db = client.db(dbname);
 
     dboper.insertDocument(db, { name: "Peter", description: "Test" },
         "dishes")
-        .then((result) => {
+        .then(result => {
             console.log("Insert Document:\n", result.ops);
 
             return dboper.findDocuments(db, "dishes");
         })
-        .then((docs) => {
+
+        .then(docs => {
             console.log("Found Documents:\n", docs);
 
             return dboper.updateDocument(db, { name: "Peter" },
                 { description: "Updated Test" }, "dishes");
-
         })
+
         .then((result) => {
             console.log("Updated Document:\n", result.result);
 
             return dboper.findDocuments(db, "dishes");
         })
+
         .then((docs) => {
             console.log("Found Updated Documents:\n", docs);
 
             return db.dropCollection("dishes");
         })
+
         .then((result) => {
             console.log("Dropped Collection: ", result);
 
